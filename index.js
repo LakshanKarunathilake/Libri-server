7cfa6027a89e5705a85b83a4a824381220268a3e
Unknown <manualsplash@gmail.com>
2019-03-23 22:34:11 +0530

    Change firebase-admin project to firebase-cli project

--- index.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-const adminKey = require('./firebase-key.json')
-
-admin.initializeApp({credential:admin.credential.cert(adminKey)})
-
-const db = admin.firestore();
