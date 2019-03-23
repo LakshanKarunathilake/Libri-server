11dcfd3a2cd5062523ec4c4d5c4bf22283834100
Unknown <manualsplash@gmail.com>
2019-03-23 23:36:10 +0530

    Add sample creation of user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,28 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// Initializing Firebase App
+admin.initializeApp();
+
+// Basic helloworld function
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+
+export const createNewUser = admin.auth().createUser({
+    email: "user@example.com",
+    emailVerified: false,
+    phoneNumber: "+11234567890",
+    password: "secretPassword",
+    displayName: "John Doe",
+    photoURL: "http://www.example.com/12345678/photo.png",
+    disabled: false
+  })
+    .then(function(userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log("Successfully created new user:", userRecord.uid);
+    })
+    .catch(function(error) {
+      console.log("Error creating new user:", error);
+    });
