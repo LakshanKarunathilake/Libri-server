b4084cb7634a7a4a9c4efd53a21cc7c653d03fc6
Unknown <manualsplash@gmail.com>
2019-03-24 22:40:38 +0530

    Account creation is parameterized

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,22 +13,21 @@ export const createNewUser = functions.https.onRequest((request, response) => {
   admin
     .auth()
     .createUser({
-      email: "use1ra@example.com",
+      email: request.get("email"),
       emailVerified: false,
-      phoneNumber: "+112345678901",
-      password: "secretPassword",
-      displayName: "John Doe1",
+      phoneNumber: request.get("phoneNumber"),
+      password: request.get("password"),
+      displayName: request.get("displayName"),
       photoURL: "http://www.example.com/12345678/photo.png",
       disabled: false
     })
     .then(function(userRecord) {
       // See the UserRecord reference doc for the contents of userRecord.
       response.send("User Created success");
-
       console.log("Successfully created new user:", userRecord.uid);
     })
     .catch(function(error) {
       console.log("Error creating new user:", error);
-      response.send("User Creation Failed");
+      response.send(error.errorInfo.message);
     });
 });
