f2d42866b4957b3be6c2ae671229584a0d9b8d57
Lakshan Karunathilake <manualsplash@gmail.com>
2019-05-17 21:20:53 +0530

    Remove user creation on cloud & view all users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -89,34 +89,13 @@ export const captchaValidate = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.createUserDocument = functions.auth.user().onCreate(user => {
-  const {
-    displayName,
-    disabled,
-    email,
-    emailVerified,
-    phoneNumber,
-    photoURL,
-    uid
-  } = user;
-
-  admin
-    .firestore()
-    .collection("users")
-    .add({
-      displayName,
-      disabled,
-      email,
-      emailVerified,
-      phoneNumber,
-      photoURL,
-      uid
-    })
-    .then(() => {
-      console.log("Successfull updated user details" + user.email);
-    })
-    .catch(error => {
-      console.error("Error occured while registering the user details");
-      console.error(error);
-    });
-});
+export const getRegisteredUsers = functions.https.onRequest(
+  async (req, res) => {
+    const users = await admin.auth().listUsers();
+    if (users) {
+      res.send(users);
+    } else {
+      res.send("Empty");
+    }
+  }
+);
